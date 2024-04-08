bb5f7077e016ff48dfdb7d4595bcea711a771aeb
Luis Mayta <luis.mayta@gmail.com>
2024-04-08 00:12:01 -0500

    Mega Proyecto parte 1

---
+++ lib/data/mega_proyectos_data.dart
@@ -0,0 +1,10 @@
+const String mega1 = '''
+Al cumplir la RCC del Perú 40 años nos preguntamos: ¿hacia dónde vamos?, ¿hay nuevas oportunidades? ¿qué desea el Señor de nosotros hoy?. Y las respuestas del Señor no se han dejado esperar. Recordemos que: “Como discípulos de Jesucristo, nos sentimos interpelados a discernir los signos de los tiempos a la luz del Espíritu Santo, para ponernos al servicio del Reino, anunciado por Jesús.” (Aparecida 33).
+''';
+const String mega2 = '''
+Todo ello se traduce en: Ser abiertos a nuevas oportunidades y se convergen en tres palabras que marcarán nuestro servicio y toda planificación del mismo, estas son: DISCIPULADO, COMUNIÓN Y MISIÓN. Estos son los tres grandes MEGAPROYECTOS que estamos llamados a realizar desde el año 2011.  La Renovación Carismática Católica del Perú llamada a ser: Una Comunidad de Discípulos en Misión.
+''';
+const String mega3 = '''
+Los Megaproyectos son proyectos u obras de grandes dimensiones realizadas a nivel nacional. No es algo teórico sino más bien algo práctico, que se puede trabajar  y que en las diferentes diócesis se aplicarán por medio de proyectos más específicos y tomando en cuenta su realidad. Estos proyectos a nivel Diocesano tienen que ser proyectos visibles que se traducen o realizan por medio de acciones concretas.  Un proyecto es el termómetro que va a medir nuestra visión.  Con ello y apuntando a una misma dirección, vamos a poder unificar esfuerzos y continuar sembrando la Cultura de Pentecostés.
+''';
+
--- lib/screens/mega_screen.dart
+++ lib/screens/mega_screen.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
 
+import '../data/mega_proyectos_data.dart';
 import 'components/components.dart';
-class MegaScreen extends StatelessWidget{
+
+class MegaScreen extends StatelessWidget {
   static const String routeName = 'mega_screen';
   const MegaScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Mega Proyectos'),
-      bottomNavigationBar: CustomNavigatorBar(),
-      body: Center(
-        child: Text('Mega Screen'),
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Mega Proyectos'),
+      bottomNavigationBar: const CustomNavigatorBar(),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(image: AssetImage('assets/images/mega_proyecto.png')),
+                  ],
+                ),
+                SelectableText(
+                  'MEGAPROYECTOS',
+                  style: textStyle.titleMedium,
+                ),
+                const SelectableText(mega1),
+                const SelectableText('Ante las sugerencias de El Espíritu Santo y a su escucha tenemos lo siguiente:'),
+                const SelectableText('  1. Seguir creciendo como un pueblo de alabanza  adoración e intercesor.'),
+                const SelectableText('  2. Ser ambiciosos en el uso de los dones carismáticos,'),
+                const SelectableText('  3. Tener una formación profunda en la Palabra de Dios,'),
+                const SelectableText('  4. Brindar servicios en las obras de misericordia de Dios.'),
+                const SelectableText(mega2),
+                const SelectableText('¿Por qué la denominación de Megaproyectos?', style: TextStyle(fontWeight: FontWeight.bold)),
+                const SelectableText(mega3),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image(image: AssetImage('assets/images/mega_comunidad.png'),width: 100,),
+                    Image(image: AssetImage('assets/images/mega_discipulado.png'),width: 100,),
+                    Image(image: AssetImage('assets/images/mega_mision.png'),width: 100,),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
